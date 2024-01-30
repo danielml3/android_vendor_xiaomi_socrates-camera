8a921b03790b73a48e958200da6280c5582b7d36
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-30 22:17:27 +0530

    camera: Patch apk to change default gallery
    Use Google Photos instead of MIUI Gallery.

--- extract-files.sh
+++ extract-files.sh
@@ -62,7 +62,12 @@ function blob_fixup() {
             patchelf --replace-needed libgui.so libgui-xiaomi.so "${2}"
             ;;
         system/priv-app/MiuiCamera/MiuiCamera.apk)
-            split --bytes=20M -d "${2}" "${2}".part
+            tmp_dir="${EXTRACT_TMP_DIR}/MiuiCamera"
+            $APKTOOL d -q "$2" -o "$tmp_dir" -f
+            grep -rl "com.miui.gallery" "$tmp_dir" | xargs sed -i 's|"com.miui.gallery"|"com.google.android.apps.photos"|g'
+            $APKTOOL b -q "$tmp_dir" -o "$2"
+            rm -rf "$tmp_dir"
+            split --bytes=20M -d "$2" "$2".part
             ;;
     esac
 }
